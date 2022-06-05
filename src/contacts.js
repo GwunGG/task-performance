8e2f43b0aebe68dc6104b881df581db61393fa4c
GwunGG <dima_osipov_2000@bk.ru>
2022-06-05 18:00:04 +0500

    Задание по task-performance

--- src/contacts.js
+++ src/contacts.js
@@ -1,16 +1,23 @@
 const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
+const add_button = document.getElementById('add_button')
+let maxVal = 50;
+let minVal = 0
 
+add_button.addEventListener('click',addContacts)
 function addContacts() {
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < 50000; i++) {
+  for (minVal; minVal < maxVal; minVal++) {
     const child = document.createElement("div");
-    child.textContent = i;
+    child.textContent = minVal;
     child.classList.add("contact");
     fragment.appendChild(child);
   }
   contacts.appendChild(fragment);
+  maxVal += 50;
+  minVal += 0
 }
+
 
 contacts.addEventListener("scroll", (e) => {
   const items = Array.from(contacts.getElementsByClassName("contact"));
@@ -22,5 +29,3 @@ contacts.addEventListener("scroll", (e) => {
     stickyHeader.textContent = items[topItemIndex].textContent;
   }
 });
-
-addContacts();
